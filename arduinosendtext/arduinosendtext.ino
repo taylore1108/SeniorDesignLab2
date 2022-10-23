83bd888edf9db2a8e23d988fd21cb275cff45fc3
taylore1108 <82013210+taylore1108@users.noreply.github.com>
2022-10-23 18:20:25 -0500

    f

--- arduinosendtext/arduinosendtext.ino
+++ arduinosendtext/arduinosendtext.ino
@@ -4,7 +4,8 @@
 #include <WiFiClient.h>
 #include <HTTP_Method.h>
 #include <WebServer.h>
-
+#include <ESPDateTime.h>
+#include <Time.h>
 #include <ESPAsyncWebServer.h>//
 #include <Uri.h> //
 #include <SPI.h>//
@@ -12,6 +13,10 @@
  
 #define USE_SERIAL Serial
 AsyncWebServer server(80);
+
+//sms set up 
+const char* resource = "https://maker.ifttt.com/trigger/TempTrigger/with/key/fOZGnZt3fxbSPMjgMfd_Xij7h7Z5Gg_B_gQv8RhImyV";
+String messageToText = "The sengal has been interupted";
 const char* ssid  = "Galaxy S2267C8";
 const char* password = "nxzz5758";
 String numberToText = "16308006164";
@@ -54,6 +59,36 @@ bool readReceiver(){
   
 }
 
+void setupDateTime() {
+  // setup this after wifi connected
+  // you can use custom timeZone,server and timeout
+  // DateTime.setTimeZone("CST-8");
+  // DateTime.setServer("asia.pool.ntp.org");
+  // DateTime.begin(15 * 1000);
+  // from
+  /** changed from 0.2.x **/
+  DateTime.setTimeZone("CST-8");
+  // this method config ntp and wait for time sync
+  // default timeout is 10 seconds
+  DateTime.begin(/* timeout param */);
+  if (!DateTime.isTimeValid()) {
+    Serial.println("Failed to get time from server.");
+  }
+}
+
+String makeMessage(){
+  String m;
+  time_t t= now();
+
+  if(isAm(t)){
+    m = "AM";
+  }
+  else{
+    m = "PM";
+  }
+  String s =  "Critical Safety Event at " + hourFormat12(t) + "\:" + minute(t) + "  " + m + " on " + month(t) + "\/" + day(t) + "\/2022";
+  return s;
+}
 
 
 void sendText(){
